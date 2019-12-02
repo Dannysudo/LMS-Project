653092796fff97c974b73a810ede9e8632a4aa7d
Dannysudo <54330310+Dannysudo@users.noreply.github.com>
2019-12-01 21:18:14 -0600

    start

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\danny\desktop\teach-me-django-dependabot-pip-djangorestframework-3.9.1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
